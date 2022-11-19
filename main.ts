d9054deadc8beda80843441830766bbadbe25692
rin-rin555 <118585882+rin-rin555@users.noreply.github.com>
2022-11-19 15:09:08 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    linebeacon.startWithDeviceMessage(
+    "wirYUiN27w4PuCFfdxGjdk9SC6MxC3RgS7dVNIdWLeT",
+    "welcome"
+    )
+})
+linebeacon.start("wirYUiN27w4PuCFfdxGjdk9SC6MxC3RgS7dVNIdWLeT")
